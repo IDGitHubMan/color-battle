3b78d4d994acc1459e83c2c158a13469b81578a8
Anthony Huarneck <anthonyhuarneck@anthonys-air.myfiosgateway.com>
2023-08-21 21:42:33 -0400

    Creates Header.

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Header = () => {
+
+    return (
+        <div>
+            <h1>Color Battle</h1>
+            <span>{damageScore}</span>
+        </div>
+    );
+}
+
+export default Header;
