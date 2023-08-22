a7d447bd04168f2befb021ca1ae7bce071a16b4a
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-08-21 21:51:13 -0400

    Create basic structure of component.

---
+++ src/components/Me.jsx
@@ -0,0 +1,9 @@
+export default function Me({ damageScore, setDamageScore }) {
+  return (
+    <div>
+      <h2>Me</h2>
+      <h4>Hitpoints: </h4>
+      <button>Attack!</button>
+    </div>
+  );
+}
