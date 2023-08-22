d093c094b1c15e988b23004be07643e2c272ce26
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-08-21 21:56:18 -0400

    Add Me component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Me from "./components/Me";
 
 function App() {
-  
-  const [damageScore, setDamageScore] = useState(0)
+  const [damageScore, setDamageScore] = useState(0);
 
   return (
     <>
-    <Header damageScore={damageScore}/>
+      <Header damageScore={damageScore} />
+      <Me damageScore={damageScore} setDamageScore={setDamageScore} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
