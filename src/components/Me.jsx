39fc7e9122740ff80c120860da313f9eb249a96c
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-08-21 21:55:49 -0400

    Create handler.

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 export default function Me({ damageScore, setDamageScore }) {
+  const [hitpoints, setHitpoints] = useState(20);
+  const attack = () => {
+    setDamageScore(Math.random() * 10);
+    setHitpoints(hitpoints - damageScore);
+  };
   return (
     <div>
       <h2>Me</h2>
-      <h4>Hitpoints: </h4>
+      <h4>Hitpoints: {hitpoints}</h4>
       <button>Attack!</button>
     </div>
   );
